5dbe6943021258c22ee17c4793459e83dc0ed316
tgilbert12990 <33769437+tgilbert12990@users.noreply.github.com>
2017-12-06 19:48:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,14 +5,14 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/schedule/ ;  botRegexDL = /^\/!rules/;
-      botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
-      botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
-      botRegexSiege = /^\/siege/; botRegexOW = /^\/ratings/; 
-      botRegexSlut = /^\/slut/; botRegexMason = /^\/mason/;
-      botRegexProp = /^\/prop/;botRegexKys = /^\/kys/; botRegexSlam = /^\/slam/; botRegexBrye = /^\/brye/;
-      botRegexGian = /^\/gian/; botRegexScrotes = /^\/scrotes/; botRegexShaki = /^\/shaki/;
-      botRegexDaf = /^\/dafuq/; botRegexMA32 = /^\/pending/; botRegexTrade = /^\/trade/; 
+      botRegex = /^\/schedule/;botRegexDL = /^\/rules/;
+      botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/;botRegexSC = /^\/SDL/i;botODB = /(.*\s+)(.*odb)(\s+.*)/i;botDuck = /^\/duck/;
+      botRegexP = /^\/PDL/i; botRegexTw = /^\/twitch/i;botRegexSb = /^\/sub/;botRegexSh = /^\/shrug/;botRegexWk = /^\/users/;botRegexCC = /^\/cc/;
+      botRegexSiege = /^\/siege/;botRegexOW = /^\/ratings/; 
+      botRegexSlut = /^\/slut/;botRegexMason = /^\/mason/;
+      botRegexProp = /^\/prop/;botRegexKys = /^\/kys/;botRegexSlam = /^\/slam/;botRegexBrye = /^\/brye/;
+      botRegexGian = /^\/gian/;botRegexScrotes = /^\/scrotes/;botRegexShaki = /^\/shaki/;
+      botRegexDaf = /^\/dafuq/;botRegexMA32 = /^\/pending/;botRegexTrade = /^\/trade/; 
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
